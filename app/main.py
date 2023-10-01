408da5f75f0d113b26588e6ae61bac7de34e6d0b
Danny Fuentes <dannyfuf@gmail.com>
2023-09-30 21:27:32 -0300

    load discord token from env variables

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+import os
 from typing import Union
 from fastapi import FastAPI
 from asyncio import create_task
+from dotenv import load_dotenv
+load_dotenv()
 
 # custom modules
 from routers.users import users
@@ -20,6 +23,6 @@ def read_root():
     return {"description": "Farm Middleware"}
 
 async def start_bot():
-    await bot.start("")
+    await bot.start(os.getenv("DISCORD_TOKEN"))
 
 create_task(start_bot())
