3d2b054aabd2e6cdb6782c8f82f416d2019d2d04
Danny Fuentes <dannyfuf@gmail.com>
2023-10-02 00:22:31 -0300

    sending request with data when commands arrives

--- app/discord/commands/marketplace.py
+++ app/discord/commands/marketplace.py
@@ -1,4 +1,18 @@
+import os
+from httpx import AsyncClient
 from discord.ext import commands
+import logging
+
+# custom modules
+from discord.command_parser import command_parser
+from discord.cache import cache_get
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+)
+
+user_host = os.getenv('USER_HOST')
 
 class MarketplaceCommands(commands.Cog):
     def __init__(self, bot):
@@ -6,10 +20,37 @@ class MarketplaceCommands(commands.Cog):
 
     @commands.command()
     async def buy(self, ctx):
-        item = ctx.message.content.split()[1::]
-        await ctx.send(f'You have bought {item}!')
+        user = ctx.message.author.name
+        action_item, action_amount = command_parser(ctx.message.content)
+        logging.info(f'buy: user: {user}, action_item: {action_item}, action_amount: {action_amount}')
+        auth_token = cache_get(user)
+        async with AsyncClient() as client:
+            res = await client.post(
+                f'{user_host}/marketplace',
+                json={
+                    'auth_token': auth_token,
+                    'action': 'buy',
+                    'action_item': action_item,
+                    'action_amount': action_amount
+                }
+            )
+            await ctx.send(f'buying...')
+
     
     @commands.command()
     async def sell(self, ctx):
-        item = ctx.message.content.split()[1::]
-        await ctx.send(f'You have sold {item}!')
+        user = ctx.message.author.name
+        action_item, action_amount = command_parser(ctx.message.content)
+        logging.info(f'sell: user: {user}, action_item: {action_item}, action_amount: {action_amount}')
+        auth_token = cache_get(user)
+        async with AsyncClient() as client:
+            res = await client.post(
+                f'{user_host}/marketplace',
+                json={
+                    'auth_token': auth_token,
+                    'action': 'sell',
+                    'action_item': action_item,
+                    'action_amount': action_amount
+                }
+            )
+            await ctx.send(f'selling...')
--- app/discord/commands/rendering.py
+++ app/discord/commands/rendering.py
@@ -1,4 +1,17 @@
+import os
+from httpx import AsyncClient
 from discord.ext import commands
+import logging
+
+# custom modules
+from discord.cache import cache_get
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+)
+
+user_host = os.getenv('USER_HOST')
 
 class RenderingCommands(commands.Cog):
     def __init__(self, bot):
@@ -6,4 +19,9 @@ class RenderingCommands(commands.Cog):
 
     @commands.command()
     async def render(self, ctx):
-        await ctx.send(f'this is your farm!')
+        user = ctx.message.author.name
+        auth_token = cache_get(user)
+        logging.info(f'render: user: {user}, auth_token: {auth_token}')
+        async with AsyncClient() as client:
+            res = await client.post(f'{user_host}/render', json={ 'user_id': user, 'auth_token': auth_token })
+            await ctx.send(f'Hey `{user}`!, your farm is being render.')
