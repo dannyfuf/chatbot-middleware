9b630f17f1a415d85ee60f6d9d48b15a361dc3dd
javier jaure <javier.jaure@usm.cl>
2023-10-29 17:28:33 -0300

    chat ejemplo

---
+++ chat_example/react-client/src/ChatScreen.jsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import useHistoryQuery from "./useHistoryQuery";
+import useMessageSubscription from "./useMessageSubscription";
+import useReplyMutation from "./useReplyMutation";
+import useHandleSendMessage from "./useHandleSendMessage";
+
+function ChatScreen({ username }) {
+  const [messages, setMessages] = React.useState([]);
+  const [reply, setReply] = React.useState("");
+
+  const [sendAPI] = useHandleSendMessage();
+  const [sendReply] = useReplyMutation();
+  const { data } = useHistoryQuery();
+
+  React.useEffect(() => {
+    if (data) setMessages(data.history);
+  }, [data]);
+
+  useMessageSubscription(({ data }) => {
+    setMessages((state) => {
+      return [...state, data.data.message];
+    });
+  });
+
+
+
+  return (
+    <form
+      className="chat-screen"
+      onSubmit={(ev) => {
+        const cleanReply = reply.trim();
+        if (cleanReply.length) {
+          if (cleanReply[0] === "!" && cleanReply.length !== 1) {
+            useHandleSendMessage(cleanReply.slice(1))
+              .then((resp) => {
+                sendReply({
+                  variables: { sender: "Servidor", message: resp },
+                });
+            })
+
+          }
+          setReply("");
+          try {
+            sendReply({
+              variables: { sender: username, message: reply },
+            });
+          } catch (error) { }
+        }
+        ev.preventDefault();
+        return false;
+      }}
+    >
+      <div className="chat-messages">
+        {messages.map(({ sender, color, message, timestamp }) => (
+          <div className="chat-message" key={timestamp}>
+            <div className="chat-message-header">
+              <strong style={{ color }}>{sender}</strong>
+              <span>{new Date(timestamp).toLocaleString("es-Es")}</span>:
+            </div>
+            <div>{message}</div>
+          </div>
+        ))}
+      </div>
+      <div className="chat-reply">
+        <input
+          type="text"
+          placeholder={"Escriba un mensaje"}
+          value={reply}
+          onChange={(ev) => setReply(ev.target.value)}
+        />
+      </div>
+    </form>
+  );
+}
+
+export default ChatScreen;
---
+++ chat_example/react-client/src/useHandleSendMessage.js
@@ -0,0 +1,111 @@
+
+function useHandleSendMessage(message) {
+  if (!message){
+    return ""
+  }
+  const msj = message.split(' ')
+  
+  let endpoint = msj[0]
+  let url = 'http://localhost:5000/' + endpoint
+  if (endpoint === 'marketplace') {
+    if (msj.length !== 3) {
+      return new Promise((resolve, reject) => {
+        resolve('Faltan Datos para realizar la accion marketplace, !marketplace item amount');
+      });
+    }
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "auth_token": "mi_token_secreto",
+        "action": endpoint,
+        "action_item": msj[1],
+        "action_amount": msj[2]
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log('Mensaje enviado con éxito');
+          return 'Compra realizada con exito'
+        } else {
+          console.error('Error al enviar el mensaje');
+          return 'Error al enviar el mensaje'
+        }
+      })
+      .catch((error) => {
+        console.error('Error en la solicitud:', error);
+        return 'Error al enviar el mensaje'
+      });
+  } else if (endpoint === 'farm') {
+    if (msj.length !== 3) {
+      return new Promise((resolve, reject) => {
+        resolve('Faltan Datos para realizar la accion farm, !farm item amount');
+      });
+
+    }
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "auth_token": "mi_token_secreto",
+        "action": endpoint,
+        "action_item": msj[1],
+        "action_amount": msj[2]
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log('Mensaje enviado con éxito');
+          return 'Plantacion realizada con exito'
+        } else {
+          console.error('Error al enviar el mensaje');
+          return 'Error al enviar el mensaje'
+        }
+      })
+      .catch((error) => {
+        console.error('Error en la solicitud:', error);
+        return 'Error al enviar el mensaje'
+      });
+  } else if (endpoint === 'render') {
+    if (msj.length !== 1) {
+      return new Promise((resolve, reject) => {
+        resolve('Comando equivocado, para crear un render escribir !render');
+      });
+    }
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "user_id": "user_id",
+        "auth_token": "mi_token_secreto"
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log('Mensaje enviado con éxito');
+          return 'Render creado con exito'
+        } else {
+          console.error('Error al enviar el mensaje');
+          return 'Error al enviar el mensaje'
+        }
+      })
+      .catch((error) => {
+        console.error('Error en la solicitud:', error);
+        return 'Error al enviar el mensaje'
+      });
+  } else {
+    return new Promise((resolve, reject) => {
+      resolve('Metodo de no valido');
+    });
+  }
+
+}
+
+
+export default useHandleSendMessage;
